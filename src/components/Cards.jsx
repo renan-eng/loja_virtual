f141fbac997155e8c6277a98be4dcc8808cdd159
renan-eng <renan3838@gmail.com>
2022-12-07 10:19:54 -0300

    Fixed card bootstrap height

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,7 +6,7 @@ const Cards = (props) => {
         <>
             {/* Primeiro Item Lista de Produtos */}
             <div className='col-12 col-sm-12 col-md-6 col-lg-3 col-xl-3 col-xxl-3 mb-4'>
-                <div className="card h100 shadow">
+                <div className="card h-100 shadow rounded">
                     <img src={props.img} alt="" className="card-img-top img-fluid" />
                     <div className="card-body">
                         <h5 className="card-title">{props.title}</h5>
