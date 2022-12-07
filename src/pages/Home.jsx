4ed520c28695036aaf9d4a122326e7fede127cdf
renan-eng <renan3838@gmail.com>
2022-12-07 05:58:42 -0300

    Create React App
    Loja Virtual Inicializada
    Instalado React-Bootstrap

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className='container-fluid'>
+            <h3 className='text-center mt-5 text-uppercase'>Loja Virtual</h3>
+            <div className="container py-4">
+                <div className="row">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
