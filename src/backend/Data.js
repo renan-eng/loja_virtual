b2df7053f53a5103c6a7e41ab6ca0337d6e33c73
renan-eng <renan3838@gmail.com>
2022-12-07 06:44:48 -0300

    Estrutura de dados que envia dados para os produtos

---
+++ src/backend/Data.js
@@ -0,0 +1,39 @@
+import primeira_img from './imagens/1_1.jpg';
+import segunda_img from './imagens/2_1.jpg';
+import terceira_img from './imagens/3_1.jpg';
+import quarta_img from './imagens/4_1.jpg';
+
+const data = {
+    produtos: [
+        {
+            id: 1,
+            titulo: "Produto Um",
+            img: primeira_img,
+            desc: "",
+            preco: 80,
+        },
+        {
+            id: 2,
+            titulo: "Produto Dois",
+            img: segunda_img,
+            desc: "",
+            preco: 50,
+        },
+        {
+            id: 3,
+            titulo: "Produto Tres",
+            img: terceira_img,
+            desc: "",
+            preco: 30,
+        },
+        {
+            id: 4,
+            titulo: "Produto Quatro",
+            img: quarta_img,
+            desc: "",
+            preco: 100,
+        },
+    ]
+}
+
+export default data
