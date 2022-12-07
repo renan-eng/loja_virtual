580a66f3bdb89e3d729e05ea1494c2a3fab17a92
renan-eng <renan3838@gmail.com>
2022-12-07 10:17:29 -0300

    Added react-use-cart to App
    https://www.npmjs.com/package/react-use-cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
+import Carrinho from './components/CarrinhoDeCompras';
+import { CartProvider } from 'react-use-cart';
 
 function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+	return (
+		<div>
+			<CartProvider>
+				<Home />
+				<Carrinho />
+			</CartProvider>
+		</div>
+	);
 }
 
 export default App;
