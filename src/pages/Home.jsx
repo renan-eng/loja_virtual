f770034889a3a86e6b85971c3391b2c39b6ec606
renan-eng <renan3838@gmail.com>
2022-12-07 06:45:46 -0300

    Import Cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import Cards from '../components/Cards';
+import data from "../backend/Data";
+import './Home.css';
 
 const Home = () => {
     return (
@@ -7,7 +9,7 @@ const Home = () => {
             <h3 className='text-center mt-5 text-uppercase'>Loja Virtual</h3>
             <div className="container py-4">
                 <div className="row">
-
+                    <Cards img="" title="title" desc="desc" />
                 </div>
             </div>
         </div>
