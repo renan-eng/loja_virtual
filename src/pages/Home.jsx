5f0ebaab676b04776350e3719d66b69f3c68b057
renan-eng <renan3838@gmail.com>
2022-12-07 14:46:46 -0300

    Corrigindo bug no nome do atributo preco

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                 <div className="row">
                     {data.produtos.map((item, index) => {
                         return (
-                            <Cards img={item.img} title={item.titulo} desc={item.desc} preco={item.preco} item={item} key={index} />
+                            <Cards img={item.img} title={item.titulo} desc={item.desc} price={item.price} item={item} key={index} />
                         )
                     })}
 
